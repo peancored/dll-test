e0bd22b8f597a911bdef19d47dc5e73efb2eab29
Anton Korolkov <toshegg190494@gmail.com>
2020-02-25 10:08:07 +0000

    Final 2

--- sample-project/webpack.config.js
+++ sample-project/webpack.config.js
@@ -4,7 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
